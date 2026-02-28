b93b87043b3e98031eb6524e04c91635b6844a0b
d798080 <151854280+d798080@users.noreply.github.com>
2026-02-28 08:54:18 +0100

    je ne sais plus

--- src/main/java/com/dauphine/blogger/controllers/CategoryController.java
+++ src/main/java/com/dauphine/blogger/controllers/CategoryController.java
@@ -3,8 +3,10 @@ package com.dauphine.blogger.controllers;
 import com.dauphine.blogger.models.Category;
 import com.dauphine.blogger.services.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -29,9 +31,19 @@ public class CategoryController {
         return categoryService.getById(id);
     }
 
+
+
+    //@PostMapping
+    //public Category createCategory(@RequestBody String name) {
+      //  return categoryService.create(name);
+    //}
+
     @PostMapping
-    public Category createCategory(@RequestBody String name) {
-        return categoryService.create(name);
+    public ResponseEntity<Category> createCategory(@RequestBody String name) {
+        Category category = categoryService.create(name);
+        return ResponseEntity
+                .created(URI.create("/v1/categories/" + category.getId()))
+                .body(category);
     }
 
     @PutMapping("/{id}")
@@ -49,10 +61,10 @@ public class CategoryController {
             summary = "Get all categories",
             description = "retrieve all categories or filter like name"
     )
-    public List<Category> getAll(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<Category>> getAll(@RequestParam(required = false) String name) {
         List<Category> categories = name == null || name.isBlank()
                 ? categoryService.getAll()
                 : categoryService.getALLLikeName(name);
-        return categories;
+        return ResponseEntity.ok(categories);
     }
 }
